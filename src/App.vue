462e624f160275d6355ab846a245815a9c24fd49
Dmytro Tverdokhlib <ditverd@gmail.com>
2022-09-01 19:05:21 +0300

    wrap

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,22 @@
 
 import Numpad from './components/numpad/Numpad.vue'
 
+import {ref, watch} from 'vue'
+
+const answer = ref(null)
+
+const onAnswer = (value: number | null) => console.log('the answer is ' + value);
+
+watch(answer, (n, o) => console.log('change detected', n, o))
+
 </script>
 
 <template>
   <div>
 
-    <Numpad/>
+    <h2>{{ answer || '...' }}</h2>
+
+    <Numpad v-model="answer" @submit="onAnswer"/>
 
   </div>
 
