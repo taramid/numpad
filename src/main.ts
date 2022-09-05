6810316634f43f1385769ee7ed6cf0b9f99df1bb
Dmytro Tverdokhlib <ditverd@gmail.com>
2022-09-05 08:01:42 +0300

    - pinia

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,4 @@
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
 import App from './App.vue'
 
 import './index.css'
@@ -7,7 +6,5 @@ import './index.css'
 import './assets/main.css'
 
 const app = createApp(App)
-
-app.use(createPinia())
 
 app.mount('#app')
